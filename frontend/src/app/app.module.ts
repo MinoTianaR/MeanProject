625fb94cdd9edf2cad340f38a5e8f43c1871fe93
MinoTianaR <s.mino.tiana@gmail.com>
2023-01-25 13:51:16 +0300

    first page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarViewComponent } from './pages/car-view/car-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarViewComponent
   ],
   imports: [
     BrowserModule,
