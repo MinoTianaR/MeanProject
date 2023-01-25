f63298164cf5b756f78ed2205d4f74a8b5494130
MinoTianaR <s.mino.tiana@gmail.com>
2023-01-25 14:18:09 +0300

    car view

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarViewComponent } from './pages/car-view/car-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: CarViewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
